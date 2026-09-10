da770662bfb7b813b4ca11704849aa30618bf37b
mayankahuja <mayankahuja785@gmail.com>
2026-09-10 23:16:40 +0530

    feat: update baseurl

--- customer_app/lib/core/constants/api_constants.dart
+++ customer_app/lib/core/constants/api_constants.dart
@@ -1,12 +1,12 @@
 class ApiConstants {
-  // static const String baseUrl = "http://127.0.0.1:8000/";
-  static const String baseUrl = "http://10.0.2.2:8000";
+  static const String baseUrl = "http://127.0.0.1:8000/";
+  // static const String baseUrl = "http://10.0.2.2:8000";
 
-  static const String login = "/auth/login";
-  static const String register = "/auth/register";
-  static const String me = "/auth/me";
+  static const String login = "auth/login";
+  static const String register = "auth/register";
+  static const String me = "auth/me";
 
-  static const String rides = "/rides";
+  static const String rides = "rides";
 
-  static const String fcmToken = "/notifications/token";
+  static const String fcmToken = "notifications/token";
 }
