a17725cd894e0ebd2a166702e6390c1539a7572e
mayankahuja <mayankahuja785@gmail.com>
2026-09-10 23:45:21 +0530

    feat: implement home screen with logout functionality

--- customer_app/lib/screens/home/home_screen.dart
+++ customer_app/lib/screens/home/home_screen.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
-
+import '../../services/auth_service.dart';
+import '../auth/login_screen.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
+
+  Future<void> logout() async {
+  await AuthService().clearToken();
+
+  if (!context.mounted) return;
+
+  Navigator.pushAndRemoveUntil(
+    context,
+    MaterialPageRoute(
+      builder: (_) => const LoginScreen(),
+    ),
+    (route) => false,
+  );
+}
     return Scaffold(
       appBar: AppBar(
         title: const Text("RideX"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: logout,
+          ),
+        ],
       ),
       body: const Center(
         child: Text(
