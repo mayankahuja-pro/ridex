f4951512d8660874c9e40b24d729d09581d4811f
mayankahuja <mayankahuja785@gmail.com>
2026-09-12 12:21:01 +0530

    feat:create ride buttom connected with create ride api

--- customer_app/lib/screens/ride/booking_screen.dart
+++ customer_app/lib/screens/ride/booking_screen.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'package:latlong2/latlong.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'map_screen.dart';
+import 'dart:convert';
+
+import '../../core/constants/api_constants.dart';
+import '../../models/ride.dart';
+import '../../services/api_service.dart';
+import '../../services/auth_service.dart';
+
 
 class BookingScreen extends StatefulWidget {
   final Position currentPosition;
+
+  
 
   const BookingScreen({
     super.key,
     required this.currentPosition,
   });
+
 
   @override
   State<BookingScreen> createState() =>
@@ -19,9 +29,89 @@ class BookingScreen extends StatefulWidget {
 
 class _BookingScreenState extends State<BookingScreen> {
 
+  final apiService = ApiService();
+  final authService = AuthService();
+
+  Ride? createdRide;
+
+
   LatLng? destinationLocation;
 
   bool isLoading = false;
+
+  Future<void> createRide() async {
+  if (destinationLocation == null) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text("Please select destination"),
+      ),
+    );
+    return;
+  }
+
+  setState(() {
+    isLoading = true;
+  });
+
+  try {
+    final token = await authService.getToken();
+
+    if (token == null) {
+      throw Exception("Authentication token missing");
+    }
+
+    final response = await apiService.post(
+      ApiConstants.rides,
+      {
+        "pickup_lat":
+            widget.currentPosition.latitude,
+        "pickup_lng":
+            widget.currentPosition.longitude,
+        "destination_lat":
+            destinationLocation!.latitude,
+        "destination_lng":
+            destinationLocation!.longitude,
+      },
+      token: token,
+    );
+
+    if (response.statusCode == 201) {
+      final data = jsonDecode(response.body);
+
+      final ride = Ride.fromJson(data);
+
+      setState(() {
+        createdRide = ride;
+      });
+
+      print("Ride created: ${ride.id}");
+      print("Fare: ₹${ride.fare}");
+      print("Status: ${ride.status}");
+    } else {
+      final data = jsonDecode(response.body);
+
+      showMessage(
+        data["detail"] ?? "Unable to create ride",
+      );
+    }
+  } catch (e) {
+    showMessage("Something went wrong");
+  } finally {
+    if (mounted) {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+}
+
+void showMessage(String message) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(message),
+    ),
+  );
+}
 
   Future<void> chooseDestination() async {
     final result = await Navigator.push(
@@ -109,15 +199,15 @@ class _BookingScreenState extends State<BookingScreen> {
             const Spacer(),
 
             SizedBox(
-              width: double.infinity,
-              height: 52,
-              child: ElevatedButton(
-                onPressed: continueBooking,
-                child: const Text(
-                  "Continue",
-                ),
-              ),
-            ),
+          width: double.infinity,
+          height: 52,
+          child: ElevatedButton(
+            onPressed: isLoading ? null : createRide,
+            child: isLoading
+                ? const CircularProgressIndicator()
+                : const Text("Book Ride"),
+          ),
+        ),
           ],
         ),
       ),
